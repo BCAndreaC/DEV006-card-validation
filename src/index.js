6caa3718960412c3d03c6cf79bfcfbe8ae34ef9a
BCAndreaC <126895867+BCAndreaC@users.noreply.github.com>
2023-03-18 14:01:30 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import validator from './validator.js';
 
-console.log(validator);
+const creditCardNumber2 = '1234567890';
+const isValid2 = validator.isValid(creditCardNumber2);
+console.log(isValid2);
+
+const creditCardNumber3 = '5200219108562870';
+const isValid3 = validator.isValid(creditCardNumber3);
+console.log(isValid3);
+
+
+function validarTC() {
+  const creditCardNumber = document.getElementsByName("tarjeta")[0].value;
+  const oculta = validator.maskify(creditCardNumber);
+  const valida = validator.isValid(creditCardNumber);
+
+  let mensaje;
+  if(valida){
+    mensaje = "Tu tarjeta " + oculta + " se ha validado, te agradecemos tu donacion!";
+  } else {
+    mensaje = "Lo sentimos tu tarjeta " + oculta + " no hemos podido validarla, por favor prueba con otra tarjeta."
+  }
+  alert(mensaje);
+}
+
+const validarBtn = document.getElementById("btn__donar");
+validarBtn.addEventListener("click", function () {
+  const creditCardNumber = document.getElementsByName("tarjeta")[0].value;
+  if (creditCardNumber.trim() === '') {
+    alert("Por favor, ingrese el número de su tarjeta de crédito.");
+    return;
+  }
+  validarTC();
+});
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,29 @@
 const validator = {
-  // ...
+  isValid: function (creditCardNumber) {
+    let sumDoubled = 0;
+    let sumNotDoubled = 0;
+
+    for (let i = creditCardNumber.length - 1; i >= 0; i--) {
+      let digit = parseInt(creditCardNumber.charAt(i));
+      if ((creditCardNumber.length - i) % 2 === 0) {
+        if ((digit *= 2) > 9) digit -= 9;
+        sumDoubled += digit;
+      } else {
+        sumNotDoubled += digit;
+      }
+    }
+    const totalSum = sumDoubled + sumNotDoubled;
+    console.log(totalSum); // Log de total sum
+    return totalSum % 10 === 0;
+  },
+
+  maskify: function (creditCardNumber) {
+    const x = creditCardNumber.split("");
+    for (let i = 0; i < x.length - 4; i++) {
+      x[i] = "#";
+    }
+    return x.join("");
+  },
 };
 
 export default validator;
